73090b6ba6064116e0be20e4b49ebf7b0e8282ea
mpdev2311 <marcospdev2311gmail.com>
2022-03-02 15:24:21 -0300

    Entendendo a execução de códigos js

---
+++ js/js.js
@@ -0,0 +1,21 @@
+// declaração de variáveis
+var nome = 'Aluno Marcos';
+var sobreNome = 'Paulo de Souza';
+
+console.log("Instrução solta em um arquivo js");
+
+// declaração de fução
+function exibirNomeConsole(){
+    console.log("exibir nome:" + nome);
+}
+exibirNomeConsole();
+function exibirNomeCompleto(){
+    console.log("Aluno, nome completo" + nome + '' + sobreNome)
+}
+
+function eventoClick(){
+    alert("você é um trouxa");
+}
+
+//declação de funçaõ
+exibirNomeConsole();
